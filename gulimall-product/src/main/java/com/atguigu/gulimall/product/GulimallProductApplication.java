420a93593e373612eb4da2dc86a909a406ca6491
DENGYONGZHEN <YONGZHENDENG111@gmail.com>
2022-12-11 19:00:15 +0900

    aliyunOSS存储测试初步完成

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ * 逻辑删除
+ *  1、配置全局的逻辑删除规则
+ *  2、配置逻辑删除的组件Bean
+ *  3、给bean加上逻辑删除注解@TableLogic
+ */
+
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,21 +1,51 @@
 package com.atguigu.gulimall.product;
 
+
+import com.aliyun.oss.OSSClient;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+  public class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+     @Autowired
+     OSSClient ossClient;
+
+  /**
+   * 文件上传步骤
+   * 1、引入starter，具体引入在common模块
+   * 2、在yml配置文件中配置相关的key，secret以及endpoint信息
+   * 3、使用OSSClient进行相关操作
+   * @throws FileNotFoundException
+   */
+  @Test
+     public void testUpload()throws FileNotFoundException{
+       InputStream inputStream = new FileInputStream("D:\\photo.jpg");
+       ossClient.putObject("gulimall-tokyo","photo.jpg",inputStream);
+       ossClient.shutdown();
+       System.out.println("上传成功");
+     }
+
+
+
+
+
+
     @Test
-    void contextLoads() {
+      public void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
 //        brandEntity.setName("华为");
 //        boolean save = brandService.save(brandEntity);
